b77002b5aa0d92b935a68f38007f7a2b4c586e7e
Valerie Loevinger <v.loevinger@gmail.com>
2020-08-19 14:23:54 -0400

    first message

--- test_repo_eclipse/src/test_repo_eclipse/Main.java
+++ test_repo_eclipse/src/test_repo_eclipse/Main.java
@@ -3,7 +3,7 @@ package test_repo_eclipse;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("My first eclipse project under git repo");
 
 	}
 
